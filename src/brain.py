e196b550bb0fa6cd615d9b9f3aa6e2824078073c
Sandheep Rajkumar <105012644+lolrazh@users.noreply.github.com>
2023-10-09 08:07:12 +0200

    Added some comments

--- src/brain.py
+++ src/brain.py
@@ -15,13 +15,13 @@ from System.Net import WebRequest, WebHeaderCollection, WebProxy, WebException
 # FUNCTION TO SEND API CALL
 def send_request(user_prompt, Description, API_Key):
     plugin_context = "You are GrasshopperAI, an AI tool working within Grasshopper to manipulate Grasshopper scripts through natural language. You will be given a description of the entire script and data about the sliders in the format <SLIDER_NAME> - <SLIDER_VALUE>. After understanding the prompt, you have to understand what slider to change and what the new slider value would be. To do that, all you have to do is call a function that I have already programmed which is change_slider_value(str(<SLIDER_NAME>), <NEW_SLIDER_VALUE>). Remember to respond only with the function and enclose the name in an str() function." # system prompt for gpt
-    url = "https://api.openai.com/v1/chat/completions"  # reference gpt model
+    url = "https://api.openai.com/v1/chat/completions"  # reference api url
     data = { # prepare data for request
-        "model": "gpt-3.5-turbo-16k",
+        "model": "gpt-3.5-turbo-16k", # gpt model, gpt-3.5 is much cheaper to run than gpt4
         "messages": [
             {
                 "role": "system",
-                "content": str(plugin_context) + "\n" + "\n" + str(Description)
+                "content": str(plugin_context) + "\n" + "\n" + str(Description)  # system prompt is set up here
             },
             {
                 "role": "user",
@@ -39,7 +39,7 @@ def send_request(user_prompt, Description, API_Key):
     request = WebRequest.Create(url) # create a web request
     request.Method = "POST"
     request.ContentType = "application/json"
-    request.Headers.Add("Authorization", "Bearer " + str(API_Key))
+    request.Headers.Add("Authorization", "Bearer " + str(API_Key)) # authentication
     request_stream = request.GetRequestStream() # write request to data stream
     request_stream.Write(Array[System.Byte](data_bytes), 0, len(data_bytes))
     request_stream.Close()
@@ -49,10 +49,10 @@ def send_request(user_prompt, Description, API_Key):
         response_reader = System.IO.StreamReader(response_stream)
         response_text = response_reader.ReadToEnd()
 
-        response_json = json.loads(response_text)
+        response_json = json.loads(response_text) # get response from openai
         return response_json
 
-    except WebException as e:
+    except WebException as e: # error handling
         display_error(str(e))
         return None
 
@@ -67,10 +67,10 @@ def construct_prompt(param_data):
     return prompt
 
 
-class Brain(component):
+class Brain(component): # set up component class
     def __new__(cls):
         instance = Grasshopper.Kernel.GH_Component.__new__(cls,
-            "Brain", "Brain", """Uses GPT to manipulate Grasshopper scripts through natural language.""", "Params", "Util")
+            "Brain", "Brain", """Uses GPT to manipulate Grasshopper scripts through natural language.""", "Params", "Util") # component info
         return instance
     
     def get_ComponentGuid(self):
@@ -113,13 +113,13 @@ class Brain(component):
         p3 = self.marshal.GetInput(DA, 3)
         result = self.RunScript(p0, p1, p2, p3)
 
-    def get_Internal_Icon_24x24(self):
+    def get_Internal_Icon_24x24(self): # component icon
         o = "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAFiUAABYlAUlSJPAAAAI6SURBVEhLvdVJyE5RHMfxa54zZYqQspAsJDZkLgtZGBYoUli9xcKwYMdKWFmYsjBmWGBhxUqRjQgrJXNKUobM0/d77jk99z3v875vj0d+9em553Tv+d9znnPvLRpMn/j7TzIBY9AVk3AFvyt+4TrGoaGsxWM4gO7jG37iAtZhYzy20FtMQ6fpi4vwouc4hZOx/QJTkWcxPsEbGmhHR0mD7URvO8guOIsZoVWme9QPw7ABXteCdjMbnrQ3tMpMgX2HQ6sopuMdLPgFLpm+w/NeYyHqxkE8aVRoFUUX3MQHjLCDOLiDPcFRrMECWNhZW9QxDsEZtspdeHHKSnjyptAql+8HVoVW/YzEPnjdETuquQOna9yWz2Dbfe9SfcV7WHg1lkbL4u9wGGd+BhaZZUfKHtjpfjdX4Yx8Fs7BYs6guuZV15AyFC7XwdCKGQ//vMuhVT48nuRWddAtGILJmIM3yIusQIpb91Z5WMtxeOL80Kr98bdDq3XcmhauFngInyXjLG+Uh7UMhmv9CP4PnvwRB5BnIqqDJ9vQAxY4hjbZDk9cH1rlVvQ14fJU41K6pHmBl3DtPV6ENvHOH8Dpb8VyWOAS5sIH8iy8w0E4gbyITqPdzIQDeIfuHPmnfYZLaHszjFs2H/wVOn0vdUOvqGc0AKMxFsa+e8gLuEz90XTSSy73FOll+dfxYfJO6xU4j6azH/UG1zw0Fb9e6c2Z8/vRdNI+r3LndPjBaSR+xZZgB3bDb3TaWf87RfEHQBfFoHsNd3QAAAAASUVORK5CYII="
         return System.Drawing.Bitmap(System.IO.MemoryStream(System.Convert.FromBase64String(o)))
 
     def RunScript(self, Parameters, API_Key, Description, Trigger):
         # FUNCTION FOR ERROR HANDLING
-        def display_error(message):
+        def display_error(message): # display error box
             rs.MessageBox(message, 16, "Error")
         # FUNCTION TO CHANGE SLIDER VALUE
         def change_slider_value(nickname, value):
@@ -129,28 +129,28 @@ class Brain(component):
                 return
             for obj in ghdoc.Objects:
                 if isinstance(obj, Grasshopper.Kernel.Special.GH_NumberSlider) and obj.NickName == nickname:
-                    if obj.Slider.Minimum <= value <= obj.Slider.Maximum:
+                    if obj.Slider.Minimum <= value <= obj.Slider.Maximum: # check slider domains
                         obj.SetSliderValue(value)
                         obj.ExpireSolution(True)
                     else:
-                        display_error("{} is out of range for '{}'.".format(value, nickname))
+                        display_error("{} is out of range for '{}'.".format(value, nickname)) # error handling
 
         # START RUNSCRIPT
         ghdoc = self.OnPingDocument()
         if not ghdoc:
-            display_error("Error accessing Grasshopper document.")
+            display_error("Error accessing Grasshopper document.") # error handling
         else:
-            input_params = self.Params.Input[0].Sources if self.Params.Input else []
-            param_data = [(param.NickName, param.CurrentValue) for param in input_params if isinstance(param, Grasshopper.Kernel.Special.GH_NumberSlider)]
+            input_params = self.Params.Input[0].Sources if self.Params.Input else [] # get component inputs
+            param_data = [(param.NickName, param.CurrentValue) for param in input_params if isinstance(param, Grasshopper.Kernel.Special.GH_NumberSlider)] # get slider data
         if param_data:
             if Trigger:
-                prompt = construct_prompt(param_data)
+                prompt = construct_prompt(param_data) # construct user prompt
                 if prompt is not None: # avoid running when user hits cancel
-                    response = send_request(prompt, Description, API_Key)
+                    response = send_request(prompt, Description, API_Key) # send api call
                     if response:  # check if response is not None
-                        function = response['choices'][0]['message']['content']
+                        function = response['choices'][0]['message']['content'] # pick out the answer from the response json
                         try:
-                            eval(str(function))
+                            eval(str(function)) # call function
                         except Exception as e:  # catch exceptions raised during the evaluation
                             display_error("Error evaluating function: {}".format(str(e)))
                     else:
@@ -173,7 +173,7 @@ class AssemblyInfo(GhPython.Assemblies.PythonAssemblyInfo):
         return "0.1"
 
     def get_AuthorName(self):
-        return "Sandheep Rajkumar"
+        return "Sandheep Rajkumar" # haha me hahaha
     
     def get_Id(self):
         return System.Guid("88adbca2-bd32-4edc-9be2-9ebf3faaf3fb")
